aecf1fb66abeb61d60a1ce714cf57084063e2485
nahid Islam Alif <nahidislamalif141@gmail.com>
2025-08-28 11:02:42 +0600

    Adding calling alert

--- script.js
+++ script.js
@@ -29,20 +29,37 @@ heartTotal("heart-click7")
 heartTotal("heart-click8")
 
 
-// copy button 
+// copy button reuseable
 
-document.getElementById("copy-btn").addEventListener('click',function(){
 
+function copyElement(id,number1){
+    document.getElementById(id).addEventListener('click',function(){
     const countCopy = document.getElementById("copy-count");
      let currentCount = parseInt(countCopy.innerText);
      currentCount++ ;
      countCopy.innerText = currentCount;
+     const number = document.getElementById(number1).innerText
+        navigator.clipboard.writeText(number).then(() => {
+            alert("Copied " + number)
 
+    })
 })
+}
+copyElement("copy-btn", "emergency-number")
+copyElement("copy-btn1", "emergency-number1")
+copyElement("copy-btn2", "emergency-number2")
+copyElement("copy-btn3", "emergency-number3")
+copyElement("copy-btn4", "emergency-number4")
+copyElement("copy-btn5", "emergency-number5")
+copyElement("copy-btn6", "emergency-number6")
+copyElement("copy-btn7", "emergency-number7")
+copyElement("copy-btn8", "emergency-number8")
+
+
 
 // Coin reuseable
 
-function coinRemain (id){
+function coinRemain (id,emergency,number1){
     document.getElementById(id).addEventListener('click',function(){
         const remainingCoin = document.getElementById("coin-count")
 
@@ -50,22 +67,27 @@ function coinRemain (id){
         alert("You don't have enough Coin")
         return ;
     }
-    alert("Calling to the police")
+    // alert("Calling to the police")
 
     let currentCoin = parseInt(remainingCoin.innerText)
     currentCoin-= 20 ;
 
     remainingCoin.innerText = currentCoin ;
 
+    const title = document.getElementById(emergency).innerText
+    const number= document.getElementById(number1).innerText
+
+    alert("📞 Calling" +" " + title +" "+ number)
+
     })
 }
-coinRemain("call-btn")
-coinRemain("call-btn1")
-coinRemain("call-btn2")
-coinRemain("call-btn3")
-coinRemain("call-btn4")
-coinRemain("call-btn5")
-coinRemain("call-btn6")
-coinRemain("call-btn7")
-coinRemain("call-btn8")
+coinRemain("call-btn","emergency-title", "emergency-number")
+coinRemain("call-btn1", "emergency-title1", "emergency-number1")
+coinRemain("call-btn2" , "emergency-title2", "emergency-number2")
+coinRemain("call-btn3", "emergency-title3", "emergency-number3")
+coinRemain("call-btn4", "emergency-title4", "emergency-number4")
+coinRemain("call-btn5" ,"emergency-title5", "emergency-number5")
+coinRemain("call-btn6" ,"emergency-title6", "emergency-number6")
+coinRemain("call-btn7" ,"emergency-title7", "emergency-number7")
+coinRemain("call-btn8" ,"emergency-title8", "emergency-number8")
 
